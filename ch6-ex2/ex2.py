b7ce67d0482d5925b9c136b992c96a0a1cedc280
ghee01 <shrkgml127@gmail.com>
2026-08-05 17:34:32 +0900

    빅분기 실기 연습

--- ch6-ex2/ex2.py
+++ ch6-ex2/ex2.py
@@ -29,15 +29,19 @@ test = pd.read_csv('hr_test.csv')
 # print(test.isnull().sum())
 # ['gender', 'enrolled_university', 'education_level', 'major_discipline', 'experience', 'company_size', 'company_type', 'last_new_job']
 
-for col in train.select_dtypes(include='object').columns:
-    train[col] = train[col].fillna(train[col].mode()[0])
-for col in test.select_dtypes(include='object').columns:
-    test[col] = test[col].fillna(test[col].mode()[0])
+# for col in train.select_dtypes(include='object').columns:
+#     train[col] = train[col].fillna(train[col].mode()[0])
+# for col in test.select_dtypes(include='object').columns:
+#     test[col] = test[col].fillna(test[col].mode()[0])
+train = train.fillna('X')
+test = test.fillna('X')
 
 # print(train.isnull().sum())
 # print(test.isnull().sum())
 
 # --- 인코딩 ---
+target = train.pop('target')
+
 data = pd.concat([train, test])
 data_oh = pd.get_dummies(data)
 train = data_oh.iloc[:len(train)]
@@ -46,8 +50,6 @@ test = data_oh.iloc[len(train):]
 # print(train.shape, test.shape)  # (15326, 188) (3832, 188)
 
 # --- 데이터 분할 ---
-target = train.pop('target')
-
 X_train, X_val, y_train, y_val = train_test_split(train, target, test_size=0.2, random_state=0)
 # print(X_train.shape, X_val.shape, y_train.shape, y_val.shape)   # (12260, 187) (3066, 187) (12260,) (3066,)
 
@@ -59,10 +61,10 @@ pred = rf.predict_proba(X_val)
 # print(pred[:10])
 
 roc_auc = roc_auc_score(y_val, pred[:, 1])
-# print(roc_auc)  # 0.7587682793206625
+# print(roc_auc)  # 0.7587682793206625 (결측치 처리 - 최빈값) / 0.7730742036233207 (결측치 처리 - "X")
 
 # --- 결과 파일 저장 ---
-# pred = rf.predict_proba(test)
-# submit = pd.DataFrame({'pred':pred[:, 1]})
-# submit.to_csv('result.csv', index=False)
-# print(pd.read_csv('result.csv').head())
+pred = rf.predict_proba(test)
+submit = pd.DataFrame({'pred':pred[:, 1]})
+submit.to_csv('result.csv', index=False)
+print(pd.read_csv('result.csv').head())
